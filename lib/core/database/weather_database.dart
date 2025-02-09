f8e886d0dd205a23d17b5bc7c2af2e81eafc926b
1Shibam <hehe.shibam@gmail.com>
2025-02-09 13:03:12 +0530

    completed database for history!!

--- lib/core/database/weather_database.dart
+++ lib/core/database/weather_database.dart
@@ -1,4 +1,3 @@
-
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:riverpod/riverpod.dart';
@@ -12,9 +11,22 @@ final weatherDataBaseProvider = FutureProvider<Database>((ref) async {
     version: 1,
     onCreate: (db, version) {
       db.execute('''
-  
-
+            CREATE TABLE weatherTable(
+            id INT PRIMARY KEY AUTOINCREMENT,
+            cityName TEXT NOT NULL,
+            temperature REAL,
+            tempMin REAL,
+            tempMax REAL,
+            humidity INT,
+            windSpeed REAL,
+            windDeg INT,
+            description TEXT,
+            cloudCoverage INT,
+            pressure INT,
+              )
 ''');
     },
   );
 });
+
+
