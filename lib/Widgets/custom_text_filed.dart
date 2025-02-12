4dfc113d9f1a6f455b846571278a151379503fbe
1Shibam <hehe.shibam@gmail.com>
2025-02-12 17:09:20 +0530

    custom text field update add controller focus node on tap and onchanged parameters

--- lib/Widgets/custom_text_filed.dart
+++ lib/Widgets/custom_text_filed.dart
@@ -1,25 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:my_weather_reader/themes/text_styles.dart';
 
-class CustomTextFiled extends ConsumerStatefulWidget {
+class CustomTextFiled extends StatelessWidget {
   final String label;
   final String hintText;
+  final TextEditingController? controller;
+  final FocusNode? focusNode;
+  final void Function()? onTap;
+  final void Function(String)? onChanged;
   const CustomTextFiled(
-      {super.key, required this.hintText, required this.label});
+      {super.key,
+      required this.hintText,
+      required this.label,
+      this.onTap,
+      this.onChanged,
+      this.controller,
+      this.focusNode});
 
-  @override
-  ConsumerState<ConsumerStatefulWidget> createState() =>
-      _CustomTextFiledState();
-}
-
-class _CustomTextFiledState extends ConsumerState<CustomTextFiled> {
   @override
   Widget build(BuildContext context) {
     return TextField(
+      controller: controller,
+      focusNode: focusNode,
+      onTap: onTap,
+      onChanged: onChanged,
       decoration: InputDecoration(
-          label: Text(widget.label),
-          hintText: widget.hintText,
+          label: Text(label),
+          hintText: hintText,
           hintStyle: AppTextStyles.heading2,
           labelStyle: AppTextStyles.heading2,
           floatingLabelStyle:
