1775a14de02019a6d016056da2af8ec5f3aba34b
1Shibam <hehe.shibam@gmail.com>
2025-02-12 17:07:44 +0530

    fixed the search service issue of data being empty

--- lib/core/services/search_service.dart
+++ lib/core/services/search_service.dart
@@ -18,27 +18,30 @@ class SearchService {
         'q': input,
         'format': 'json',
       });
+      print(response.statusCode);
 
       if (response.statusCode == 200) {
+        print('i am inside the if block');
         final List<Map<String, dynamic>> data =
             List<Map<String, dynamic>>.from(response.data);
+        print(data);
         final result =
             data.map((json) => SearchSuggestions.fromJson(json)).toList();
-
+        print(result.length);
+        print(result.isEmpty);
 
         return result;
       } else {
         return [];
       }
     } catch (e) {
+      print('emtpy ass nigga');
       return [];
     }
   }
 }
 
 void main() async {
-  final searchService = SearchService();
-
-  // Example search input, you can change this as needed
-  await searchService.placeSuggestion('delhi');
+  SearchService searchService = SearchService();
+  await searchService.placeSuggestion('nigeria');
 }
