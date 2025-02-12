59d714f253b3d585887fbbebddb0b8989b6aebbd
1Shibam <hehe.shibam@gmail.com>
2025-02-12 13:58:02 +0530

    theme update

---
+++ lib/themes/theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:my_weather_reader/themes/app_colors.dart';
+
+final appThemeData = ThemeData(
+  inputDecorationTheme: InputDecorationTheme(
+    border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10.r),
+        borderSide: BorderSide(
+            color: AppColors.lightBlueShade.withOpacity(0.6), width: 2)),
+    enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10.r),
+        borderSide: const BorderSide(color: Colors.grey, width: 2)),
+    focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(20.r),
+        borderSide: const BorderSide(color: Colors.white, width: 2)),
+  ),
+);
