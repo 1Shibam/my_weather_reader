e47d08d2ec58b3d12cf0f70f5195fdfc08a5b7d5
1Shibam <hehe.shibam@gmail.com>
2025-02-12 17:08:03 +0530

    search suggestion appears now

--- lib/models/search_suggestions.dart
+++ lib/models/search_suggestions.dart
@@ -1,20 +1,23 @@
 class SearchSuggestions {
   String? displayName;
-  String? lat;
-  String? lon;
+  double? lat;
+  double? lon;
 
   SearchSuggestions({this.displayName, this.lat, this.lon});
 
+  // Factory constructor for JSON parsing
   SearchSuggestions.fromJson(Map<String, dynamic> json) {
     displayName = json["display_name"];
-    lat = json["lat"];
-    lon = json["lon"];
+    lat = double.tryParse(json["lat"].toString()) ??
+        0.0; // Handle string to double conversion
+    lon = double.tryParse(json["lon"].toString()) ?? 0.0;
   }
 
   static List<SearchSuggestions> fromList(List<Map<String, dynamic>> list) {
-    return list.map(SearchSuggestions.fromJson).toList();
+    return list.map((json) => SearchSuggestions.fromJson(json)).toList();
   }
 
+  // To convert the object to a Map for easy serialization
   Map<String, dynamic> toJson() {
     return {
       "display_name": displayName,
@@ -23,7 +26,6 @@ class SearchSuggestions {
     };
   }
 
-  // Override toString method to display custom output
   @override
   String toString() {
     return 'SearchSuggestions(displayName: $displayName, lat: $lat, lon: $lon)';
