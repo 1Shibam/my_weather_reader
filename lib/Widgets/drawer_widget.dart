c1a8be9671501aeb8e20f992ee81d25ff77ff6a6
1Shibam <hehe.shibam@gmail.com>
2025-02-12 14:25:13 +0530

    update the drawer makes the aobut tile clickable

--- lib/Widgets/drawer_widget.dart
+++ lib/Widgets/drawer_widget.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:my_weather_reader/main.dart';
+import 'package:my_weather_reader/router/router_config.dart';
 import 'package:my_weather_reader/themes/app_colors.dart';
+import 'package:my_weather_reader/themes/text_styles.dart';
 
 class DrawerWidget extends ConsumerStatefulWidget {
   const DrawerWidget({super.key});
@@ -16,9 +21,41 @@ class _DrawerWidgetState extends ConsumerState<DrawerWidget> {
       backgroundColor: AppColors.lightBlueShade,
       child: ListView(
         children: [
-          DrawerHeader(child: Text('Menu')),
-          ListTile(title: Text('Item 1')),
-          ListTile(title: Text('Item 2')),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: DrawerHeader(
+                child: Text(
+              'Menu',
+              style: AppTextStyles.heading1
+                  .copyWith(color: Colors.black, fontSize: 40.sp),
+            )),
+          ),
+          ListTile(
+            onTap: () => context.go('/about'),
+              title: Text(
+            'About',
+            style: AppTextStyles.heading1.copyWith(color: Colors.black87),
+          )),
+          ExpansionTile(
+            
+            initiallyExpanded: true,
+            iconColor: AppColors.darkBlue,
+            title: Text(
+              'History',
+              style: AppTextStyles.heading1.copyWith(color: Colors.black87),
+            ),
+            children: [
+              SizedBox(
+                height: 30.h,
+              ),
+              const Center(
+                child: Text('There is no Search data yer!!'),
+              ),
+              SizedBox(
+                height: 30.h,
+              ),
+            ],
+          )
         ],
       ),
     );
