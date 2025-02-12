68fd9f82d48a270aad5622e16b98ab89912a967e
1Shibam <hehe.shibam@gmail.com>
2025-02-12 13:57:49 +0530

    some color changes

--- lib/themes/app_colors.dart
+++ lib/themes/app_colors.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color darkBlue = Color(0xFF03045E);
+  static const Color darkBlue = Color.fromARGB(255, 0, 52, 173);
   static const Color waterBlue = Color(0xFF0077B6);
   static const Color skyBlue = Color(0xFF00B4D8);
   static const Color lightBlueShade = Color.fromRGBO(144, 224, 239, 1);
   static const Color blueShade100 = Color(0xFFCAF0F8);
 }
-
