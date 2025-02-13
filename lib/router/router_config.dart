22492d790c50f28553f33e49b3139a91690972bf
1Shibam <hehe.shibam@gmail.com>
2025-02-13 23:43:06 +0530

    router animation added

--- lib/router/router_config.dart
+++ lib/router/router_config.dart
@@ -2,30 +2,70 @@ import 'package:go_router/go_router.dart';
 import 'package:my_weather_reader/screens/about_page.dart';
 import 'package:my_weather_reader/screens/search_screen.dart';
 import 'package:my_weather_reader/screens/weather_home_screen.dart';
+import 'package:flutter/material.dart';
 
-GoRouter router = GoRouter(initialLocation: '/', routes: [
-  GoRoute(
-    path: '/',
-    builder: (context, state) {
-      return const WeatherHomeScreen();
-    },
-  ),
-  GoRoute(
-    path: '/home',
-    builder: (context, state) {
-      return const WeatherHomeScreen();
-    },
-  ),
-  GoRoute(
-    path: '/about',
-    builder: (context, state) {
-      return const AboutPage();
-    },
-  ),
-  GoRoute(
-    path: '/search',
-    builder: (context, state) {
-      return const SearchScreen();
-    },
-  ),
-]);
+GoRouter router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      pageBuilder: (context, state) {
+        return CustomTransitionPage(
+          key: state.pageKey,
+          child: const WeatherHomeScreen(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+        );
+      },
+    ),
+    GoRoute(
+      path: '/home',
+      pageBuilder: (context, state) {
+        return CustomTransitionPage(
+          key: state.pageKey,
+          child: const WeatherHomeScreen(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+        );
+      },
+    ),
+    GoRoute(
+      path: '/about',
+      pageBuilder: (context, state) {
+        return CustomTransitionPage(
+          key: state.pageKey,
+          child: const AboutPage(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+        );
+      },
+    ),
+    GoRoute(
+      path: '/search',
+      pageBuilder: (context, state) {
+        return CustomTransitionPage(
+          key: state.pageKey,
+          child: const SearchScreen(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+        );
+      },
+    ),
+  ],
+);
