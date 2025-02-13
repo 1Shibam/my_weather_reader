34cbb586f9584094385e6b1145c427b057f6b6e3
1Shibam <hehe.shibam@gmail.com>
2025-02-13 11:44:45 +0530

    handled the state correctly this time

--- lib/providers/search_suggestions_provider.dart
+++ lib/providers/search_suggestions_provider.dart
@@ -35,3 +35,48 @@ class SearchSuggestionNotifier extends StateNotifier<List<SearchSuggestions>> {
     state = suggestions;
   }
 } */
+
+class SearchSuggestionsNotifier
+    extends StateNotifier<AsyncValue<List<SearchSuggestions>>> {
+  final SearchService searchService;
+
+  //! Popular search recommendation -
+  static List<SearchSuggestions> popularPlaces = [
+    SearchSuggestions(
+        displayName: 'New York City, USA', lat: 40.7128, lon: -74.0060),
+    SearchSuggestions(displayName: 'London, UK', lat: 51.5074, lon: -0.1278),
+    SearchSuggestions(displayName: 'Tokyo, Japan', lat: 35.6895, lon: 139.6917),
+    SearchSuggestions(displayName: 'Paris, France', lat: 48.8566, lon: 2.3522),
+    SearchSuggestions(
+        displayName: 'Sydney, Australia', lat: -33.8688, lon: 151.2093),
+    SearchSuggestions(
+        displayName: 'Cape Town, South Africa', lat: -33.9249, lon: 18.4241),
+    SearchSuggestions(
+        displayName: 'Rio de Janeiro, Brazil', lat: -22.9068, lon: -43.1729),
+    SearchSuggestions(
+        displayName: 'Moscow, Russia', lat: 55.7558, lon: 37.6176),
+  ];
+  SearchSuggestionsNotifier(this.searchService)
+      : super(AsyncValue.data(popularPlaces));
+
+  Future<void> fetchSuggestions(String query) async {
+    if (query.isEmpty) {
+      state = AsyncValue.data(popularPlaces);
+      return;
+    }
+    state = const AsyncValue.loading();
+    try {
+      final suggestions = await searchService.placeSuggestion(query);
+      state = AsyncValue.data(suggestions);
+    } catch (error, stackTrace) {
+      AsyncValue.error(error, stackTrace);
+    }
+  }
+}
+
+//providers
+final searchServiceProvider = Provider<SearchService>((ref) => SearchService());
+
+final searchSuggestionsProvider = StateNotifierProvider.autoDispose<
+        SearchSuggestionsNotifier, AsyncValue<List<SearchSuggestions>>>(
+    (ref) => SearchSuggestionsNotifier(ref.read(searchServiceProvider)));
