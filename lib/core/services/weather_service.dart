e17e25a788c4f51ec255a654c24e28332b9ef6db
1Shibam <hehe.shibam@gmail.com>
2025-02-15 00:16:40 +0530

    writing weather service methods

--- lib/core/services/weather_service.dart
+++ lib/core/services/weather_service.dart
@@ -3,3 +3,67 @@ import 'package:dio/dio.dart';
 class WeatherService {
   final Dio dio = Dio();
 }
+
+
+/*
+class WeatherState {
+  final WeatherData? weather;
+  final bool isLoading;
+  final String error;
+
+  WeatherState({
+    this.weather,
+    this.isLoading = false,
+    this.error = '',
+  });
+}
+
+class WeatherNotifier extends StateNotifier<WeatherState> {
+  WeatherNotifier() : super(WeatherState(isLoading: true));
+
+  Future<void> fetchWeatherByCoordinates(double lat, double lon) async {
+    state = WeatherState(isLoading: true);
+    try {
+      final apiKey = weatherApiKey;
+      final url =
+          'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apiKey&units=metric';
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final _weather = WeatherData.fromJson(json.decode(response.body));
+        state = WeatherState(weather: _weather, isLoading: false);
+      } else {
+        state = WeatherState(
+            error: 'Failed to load weather data', isLoading: false);
+      }
+    } catch (e) {
+      state = WeatherState(error: e.toString(), isLoading: false);
+    }
+  }
+
+  Future<void> fetchWeatherByCity(String city) async {
+    state = WeatherState(isLoading: true);
+    try {
+      final apiKey = weatherApiKey;
+      final url =
+          'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric';
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final weather = WeatherData.fromJson(json.decode(response.body));
+        state = WeatherState(weather: weather, isLoading: false);
+      } else {
+        state = WeatherState(
+            error: 'Failed to load weather data', isLoading: false);
+      }
+    } catch (e) {
+      state = WeatherState(error: e.toString(), isLoading: false);
+    }
+  }
+}
+
+final weatherProvider =
+    StateNotifierProvider<WeatherNotifier, WeatherState>((ref) {
+  return WeatherNotifier();
+});
+*/
