8ed53f5ce191204e8c1dca9814dd25a867105f37
1Shibam <hehe.shibam@gmail.com>
2025-02-15 15:42:39 +0530

    weather service provider

--- lib/providers/weather_service_provider.dart
+++ lib/providers/weather_service_provider.dart
@@ -1,12 +1,38 @@
-
 import 'package:my_weather_reader/core/services/weather_service.dart';
+import 'package:my_weather_reader/models/weather_data.dart';
 import 'package:riverpod/riverpod.dart';
+
+
+
+class WeatherServiceNotifier extends StateNotifier<AsyncValue<WeatherData>> {
+  final WeatherService service;
+
+  WeatherServiceNotifier(this.service)
+      : super(const AsyncValue.loading()); //initial state
+
+  Future<void> searchLocation(String location) async {
+    state = const AsyncValue.loading();
+    try {
+      final search = await service.searchByLocationName(location);
+      state = AsyncValue.data(search);
+    } catch (error, stackTrace) {
+      state = AsyncValue.error(error, stackTrace);
+    }
+  }
+
+  Future<void> searchCoordinates(double latitude, double longitude) async {
+    state = const AsyncValue.loading();
+    try {
+      final coordinateSearch =
+          await service.serachByCoordinates(latitude, longitude);
+      state = AsyncValue.data(coordinateSearch);
+    } catch (e, st) {
+      state = AsyncValue.error(e, st);
+    }
+  }
+}
 
 final weatherServiceProvider =
     Provider<WeatherService>((ref) => WeatherService());
-
-class WeatherServiceNotifier extends StateNotifier<AsyncValue<WeatherService>> {
-  final WeatherService service;
-
-  WeatherServiceNotifier(this.service) : super(const AsyncValue.loading()); //initial state
-}
+final weatherServiceNotifierProvider = StateNotifierProvider(
+    (ref) => WeatherServiceNotifier(ref.read(weatherServiceProvider)));
