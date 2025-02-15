ffcd9337249f7d652c7abfe5e4fa48ffe9821c42
1Shibam <hehe.shibam@gmail.com>
2025-02-15 13:47:19 +0530

    add weather service provider

---
+++ lib/providers/weather_service_provider.dart
@@ -0,0 +1,12 @@
+import 'package:my_weather_reader/core/services/search_service.dart';
+import 'package:my_weather_reader/core/services/weather_service.dart';
+import 'package:riverpod/riverpod.dart';
+
+final weatherServiceProvider =
+    Provider<WeatherService>((ref) => WeatherService());
+
+class WeatherServiceNotifier extends StateNotifier<AsyncValue<WeatherService>> {
+  final SearchService service;
+
+  WeatherServiceNotifier(this.service) : super(const AsyncValue.loading()); //initial state
+}
