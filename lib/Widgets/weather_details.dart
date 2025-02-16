c5aad373dde827a7a86c2df81403726e20fb1564
1Shibam <hehe.shibam@gmail.com>
2025-02-16 15:54:41 +0530

    minor update

--- lib/Widgets/weather_details.dart
+++ lib/Widgets/weather_details.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:my_weather_reader/Widgets/city_name.dart';
+import 'package:my_weather_reader/Widgets/weather_condition.dart';
 
 class WeatherDetails extends StatelessWidget {
   const WeatherDetails({super.key});
@@ -11,11 +12,33 @@ class WeatherDetails extends StatelessWidget {
   ) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16.w),
-      child: const Column(
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          LocationName(
+          const LocationName(
             locationName: 'Jammu And Kashmir',
+          ),
+          SizedBox(
+            height: 12.h,
+          ),
+          Row(
+            children: [
+              const Expanded(
+                child: WeatherCondition(
+                  isDayTime: true,
+                  weatherCondition: 'snow',
+                ),
+              ),
+              SizedBox(
+                width: 4.w,
+              ),
+              const Expanded(
+                child: WeatherCondition(
+                  isDayTime: true,
+                  weatherCondition: 'snow',
+                ),
+              ),
+            ],
           )
         ],
       ),
