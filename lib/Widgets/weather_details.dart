a3db83b8ccce8fc016a621c182a804e5658c1a38
1Shibam <hehe.shibam@gmail.com>
2025-02-17 11:51:06 +0530

    update weather detail widget

--- lib/Widgets/weather_details.dart
+++ lib/Widgets/weather_details.dart
@@ -6,10 +6,9 @@ import 'package:my_weather_reader/Widgets/get_weather_animation.dart';
 import 'package:my_weather_reader/Widgets/location_temperature.dart';
 import 'package:my_weather_reader/Widgets/other_weather_details_expansion_tile.dart';
 import 'package:my_weather_reader/Widgets/weather_condition.dart';
-import 'package:riverpod/riverpod.dart';
+import 'package:my_weather_reader/providers/weather_animation_provider.dart';
 
-final weatheAnimationStateProvider =
-    AutoDisposeStateProvider<bool>((ref) => true);
+
 
 class WeatherDetails extends StatelessWidget {
   const WeatherDetails({super.key});
@@ -48,7 +47,7 @@ class WeatherDetails extends StatelessWidget {
           ),
           Consumer(
             builder: (context, ref, child) {
-              final showAnimation = ref.watch(weatheAnimationStateProvider);
+              final showAnimation = ref.watch(weatherAnimationStateProvider);
               return showAnimation
                   ? Column(
                       children: [
