db2bb3aada203005579eaeda25994384abdf20fa
1Shibam <hehe.shibam@gmail.com>
2025-02-17 21:18:12 +0530

    extract the detail widget

--- lib/Widgets/weather_details.dart
+++ lib/Widgets/weather_details.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:my_weather_reader/Widgets/city_name.dart';
+import 'package:my_weather_reader/Widgets/error_state_widget.dart';
 import 'package:my_weather_reader/Widgets/get_weather_animation.dart';
 import 'package:my_weather_reader/Widgets/location_temperature.dart';
 import 'package:my_weather_reader/Widgets/other_weather_details_expansion_tile.dart';
@@ -9,6 +10,7 @@ import 'package:my_weather_reader/Widgets/weather_condition.dart';
 import 'package:my_weather_reader/providers/geo_locator_provider.dart';
 
 import 'package:my_weather_reader/providers/weather_animation_provider.dart';
+import 'package:my_weather_reader/providers/weather_service_provider.dart';
 
 class WeatherDetails extends ConsumerWidget {
   const WeatherDetails({super.key});
@@ -16,29 +18,78 @@ class WeatherDetails extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ref) {
     final geoLocator = ref.watch(geoNotifierStateProvider);
+    final weatherState = ref.watch(weatherServiceNotifierProvider);
+    return geoLocator.when(
+        data: (location) {
+          ref
+              .read(weatherServiceNotifierProvider.notifier)
+              .searchCoordinates(location.latitude, location.longitude);
+         return weatherState.when(
+              data: (data) {
+                return DetailsWidget(
+                    locationName: data.cityName,
+                    isDayTime: false,
+                    weatherCondition: data.description,
+                    tempInCelcious: data.temperature);
+              },
+              error: (error, stackTrace) {
+                return const Center(
+                  child: ErrorStateWidget(),
+                );
+              },
+              loading: () => Center(
+                    child:
+                        Image.asset('assets/animations/weather-animation.gif'),
+                  ));
+        },
+        error: (error, stackTrace) {return const Center(
+                  child: ErrorStateWidget(),
+                );},
+        loading: () => Center(
+              child: Image.asset('assets/animations/weather-animation.gif'),
+            ));
+  }
+}
+
+class DetailsWidget extends StatelessWidget {
+  final String locationName;
+  final bool isDayTime;
+  final String weatherCondition;
+  final double tempInCelcious;
+  const DetailsWidget({
+    required this.locationName,
+    required this.isDayTime,
+    required this.weatherCondition,
+    required this.tempInCelcious,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16.w),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const LocationName(
-            locationName: 'Jammu And Kashmir',
+          LocationName(
+            locationName: locationName,
           ),
           SizedBox(
             height: 12.h,
           ),
           Row(
             children: [
-              const Expanded(
+              Expanded(
                 child: WeatherCondition(
                   isDayTime: false,
-                  weatherCondition: 'clear sky hai yaar tu samjah',
+                  weatherCondition: weatherCondition,
                 ),
               ),
               SizedBox(
                 width: 12.w,
               ),
-              const Expanded(child: LocationTemperature(tempInCelcious: 43.78)),
+              Expanded(
+                  child: LocationTemperature(tempInCelcious: tempInCelcious)),
             ],
           ),
           SizedBox(
@@ -50,7 +101,7 @@ class WeatherDetails extends ConsumerWidget {
               return showAnimation
                   ? Column(
                       children: [
-                        getWeatherAnimation('snow', false),
+                        getWeatherAnimation(weatherCondition, isDayTime),
                         SizedBox(
                           height: 16.h,
                         ),
