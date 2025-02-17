99caaddc71556229a044f33cca66eef1ecb44526
1Shibam <hehe.shibam@gmail.com>
2025-02-17 11:39:26 +0530

    wrong approach deleted !!

--- lib/preferences/enable_weather_animation_preference.dart
+++
@@ -1,6 +0,0 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
-Future<void> enableWeatherAnimation(bool val) async {
-  final prefs = await SharedPreferences.getInstance();
-  prefs.setBool('isAnimationEnabled', val);
-}
