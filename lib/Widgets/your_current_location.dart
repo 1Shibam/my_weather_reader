f9d38bab139037c0aa90ab3a5b86f4cc8ace1681
1Shibam <hehe.shibam@gmail.com>
2025-02-17 08:42:18 +0530

    reanamed

--- lib/Widgets/your_current_location.dart
+++ lib/Widgets/your_current_location.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class YourCreentLocation extends ConsumerWidget {
-  const YourCreentLocation({super.key});
+class YourCurrentLocation extends ConsumerWidget {
+  const YourCurrentLocation({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
