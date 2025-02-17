3d7e7281dcf50bba10b31fba483a20495cb3d749
1Shibam <hehe.shibam@gmail.com>
2025-02-17 09:43:36 +0530

    update other details tiles

--- lib/Widgets/other_weather_details_expansion_tile.dart
+++ lib/Widgets/other_weather_details_expansion_tile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:my_weather_reader/themes/app_colors.dart';
 import 'package:my_weather_reader/themes/text_styles.dart';
 
 class OtherWeatherDetailsExpansionTile extends StatelessWidget {
@@ -6,10 +8,43 @@ class OtherWeatherDetailsExpansionTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ExpansionTile(
-      title: Text(
-        'Other Details',
-        style: AppTextStyles.heading2,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20.r),
+      child: ExpansionTile(
+        title: Text(
+          'Other Detials',
+          style: AppTextStyles.heading2,
+        ),
+        collapsedIconColor: Colors.white,
+        iconColor: Colors.white,
+        backgroundColor: AppColors.waterBlue,
+        collapsedBackgroundColor: AppColors.waterBlue,
+        collapsedShape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.r)),
+        initiallyExpanded: true,
+        children: [
+          Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 4.h),
+                child: ListTile(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.r)),
+                  title: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(),
+                      Text(
+                        'bruh - bruh',
+                        style: AppTextStyles.heading1,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
