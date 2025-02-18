94d3e28995e0840e4488b78393829af1fd53b70d
1Shibam <hehe.shibam@gmail.com>
2025-02-18 19:39:20 +0530

    warning message fixed

--- lib/Widgets/show_warning_dialog.dart
+++ lib/Widgets/show_warning_dialog.dart
@@ -1,24 +1,23 @@
-
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:my_weather_reader/themes/text_styles.dart';
 
 void showWarningDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text("NOTE",
-            style: AppTextStyles.heading1.copyWith(color: Colors.red)),
-        content: Text(
-          "The weather data is based on precise coordinates, which may result in a different display name than the suggested search location.",
-          style: AppTextStyles.heading2.copyWith(color: Colors.red),
-        ),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: Text("OK",
-                style: AppTextStyles.heading2.copyWith(color: Colors.blue)),
-          ),
-        ],
+  showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: Text("NOTE",
+          style: AppTextStyles.heading1.copyWith(color: Colors.red)),
+      content: Text(
+        "The suggested location may display different results because coordinate-based searches provide more precise locations.",
+        style: AppTextStyles.heading2.copyWith(color: Colors.red),
       ),
-    );
-  }
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: Text("OK",
+              style: AppTextStyles.heading2.copyWith(color: Colors.blue)),
+        ),
+      ],
+    ),
+  );
+}
