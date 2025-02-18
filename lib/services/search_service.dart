341830d03cf9e9dee56d97006b34f24c261eb5e3
1Shibam <hehe.shibam@gmail.com>
2025-02-18 11:03:47 +0530

    update directory

---
+++ lib/services/search_service.dart
@@ -0,0 +1,37 @@
+import 'package:dio/dio.dart';
+import 'package:my_weather_reader/api_key.dart';
+import 'package:my_weather_reader/models/search_suggestions.dart';
+
+class SearchService {
+  final Dio dio = Dio(BaseOptions(
+    baseUrl: 'https://api.locationiq.com/v1',
+    receiveTimeout: const Duration(seconds: 4),
+    sendTimeout: const Duration(seconds: 7),
+  ));
+
+  final String apiKey = mapsApiKey; // Replace with your actual API key
+
+  Future<List<SearchSuggestions>> placeSuggestion(String input) async {
+    try {
+      final response = await dio.get('/autocomplete.php', queryParameters: {
+        'key': apiKey,
+        'q': input,
+        'format': 'json',
+      });
+
+      if (response.statusCode == 200) {
+        final List<Map<String, dynamic>> data =
+            List<Map<String, dynamic>>.from(response.data);
+        final result =
+            data.map((json) => SearchSuggestions.fromJson(json)).toList();
+
+        return result;
+      } else {
+        return [];
+      }
+    } catch (e) {
+      return [];
+    }
+  }
+}
+
