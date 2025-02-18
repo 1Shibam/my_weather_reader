83ebb4bb410a519536ed2d4c6d6a454e0865bbb7
1Shibam <hehe.shibam@gmail.com>
2025-02-18 19:31:42 +0530

    add warning when suggested location

--- lib/Widgets/suggested_location_text.dart
+++ lib/Widgets/suggested_location_text.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:my_weather_reader/Widgets/show_warning_dialog.dart';
 import 'package:my_weather_reader/themes/text_styles.dart';
 
 class SuggestedLocationsText extends StatelessWidget {
@@ -9,22 +10,43 @@ class SuggestedLocationsText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Text(
-            'Suggested Location',
-            style: AppTextStyles.heading1,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        InkWell(
+          onTap: () => showWarningDialog(context),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(Icons.info_outline, color: Colors.red, size: 22.sp),
+              SizedBox(width: 6.w),
+              Text(
+                "Note",
+                style: AppTextStyles.heading2.copyWith(
+                  color: Colors.red,
+                ),
+              ),
+            ],
           ),
-          SizedBox(
-            width: 12.w,
-          ),
-          Icon(
-            Icons.location_on,
-            color: Colors.red,
-            size: 40.spMax,
-          )
-        ],
-      );
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              'Suggested Location',
+              style: AppTextStyles.heading1,
+            ),
+            SizedBox(
+              width: 12.w,
+            ),
+            Icon(
+              Icons.location_on,
+              color: Colors.red,
+              size: 40.spMax,
+            )
+          ],
+        ),
+      ],
+    );
   }
 }
