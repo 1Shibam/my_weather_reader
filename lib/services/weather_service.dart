e346439cdf0fd5bbb82e4b06a8151666d4d2240e
1Shibam <hehe.shibam@gmail.com>
2025-02-19 12:40:55 +0530

    add forecast method to the search service

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:my_weather_reader/api_key.dart';
+import 'package:my_weather_reader/models/forecast_data_model.dart';
 import 'package:my_weather_reader/models/weather_data.dart';
 
 class WeatherService {
@@ -54,68 +55,28 @@ class WeatherService {
       throw Exception(e.toString());
     }
   }
-}
 
-
-/*
-class WeatherState {
-  final WeatherData? weather;
-  final bool isLoading;
-  final String error;
-
-  WeatherState({
-    this.weather,
-    this.isLoading = false,
-    this.error = '',
-  });
-}
-
-class WeatherNotifier extends StateNotifier<WeatherState> {
-  WeatherNotifier() : super(WeatherState(isLoading: true));
-
-  Future<void> fetchWeatherByCoordinates(double lat, double lon) async {
-    state = WeatherState(isLoading: true);
+  Future<ForecastDataModel> getWeatherForecast(
+      double latitude, double longitude) async {
     try {
-      final apiKey = weatherApiKey;
-      final url =
-          'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apiKey&units=metric';
-      final response = await http.get(Uri.parse(url));
-
+      final response = await dio.get('/forecast', queryParameters: {
+        'lat': latitude,
+        'lon': longitude,
+        'appid': weatherApiKey,
+        'units': 'metric'
+      });
       if (response.statusCode == 200) {
-        final _weather = WeatherData.fromJson(json.decode(response.body));
-        state = WeatherState(weather: _weather, isLoading: false);
+        final Map<String, dynamic> forecastData =
+            Map<String, dynamic>.from(response.data);
+        final resultForecast = ForecastDataModel.fromJson(forecastData);
+        print(resultForecast);
+        return resultForecast;
+        
       } else {
-        state = WeatherState(
-            error: 'Failed to load weather data', isLoading: false);
+        throw Exception('Failet to get weather Forecast!!');
       }
     } catch (e) {
-      state = WeatherState(error: e.toString(), isLoading: false);
-    }
-  }
-
-  Future<void> fetchWeatherByCity(String city) async {
-    state = WeatherState(isLoading: true);
-    try {
-      final apiKey = weatherApiKey;
-      final url =
-          'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric';
-      final response = await http.get(Uri.parse(url));
-
-      if (response.statusCode == 200) {
-        final weather = WeatherData.fromJson(json.decode(response.body));
-        state = WeatherState(weather: weather, isLoading: false);
-      } else {
-        state = WeatherState(
-            error: 'Failed to load weather data', isLoading: false);
-      }
-    } catch (e) {
-      state = WeatherState(error: e.toString(), isLoading: false);
+      throw Exception(e.toString());
     }
   }
 }
-
-final weatherProvider =
-    StateNotifierProvider<WeatherNotifier, WeatherState>((ref) {
-  return WeatherNotifier();
-});
-*/
