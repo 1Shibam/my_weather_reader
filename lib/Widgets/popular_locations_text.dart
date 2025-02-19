51bd3d297a3c7b0f314291790dead1cf5955ddbc
1Shibam <hehe.shibam@gmail.com>
2025-02-19 12:14:20 +0530

    update popular location text using rich text

--- lib/Widgets/popular_locations_text.dart
+++ lib/Widgets/popular_locations_text.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:my_weather_reader/Widgets/show_warning_dialog.dart';
+import 'package:my_weather_reader/providers/show_serach_suggestion_preference.dart';
 
 import 'package:my_weather_reader/themes/text_styles.dart';
 
@@ -8,19 +11,59 @@ class PopularLocationsText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.end,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          'Popular Locations',
-          style: AppTextStyles.heading1,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            InkWell(
+              onTap: () => showWarningDialog(context),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.info_outline, color: Colors.red, size: 22.sp),
+                  SizedBox(width: 6.w),
+                  Text(
+                    "Note",
+                    style: AppTextStyles.heading2.copyWith(
+                      color: Colors.red,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Consumer(
+              builder: (context, ref, child) {
+                final value = ref.watch(searchSuggestionEnableProvider);
+                return Switch(
+                  activeColor: Colors.blue,
+                  value: value,
+                  onChanged: (currentVal) {
+                    ref
+                        .read(searchSuggestionEnableProvider.notifier)
+                        .setSerachState(currentVal);
+                  },
+                );
+              },
+            )
+          ],
         ),
-        SizedBox(width: 12.w),
-        Icon(
-          Icons.location_on,
-          color: Colors.red,
-          size: 40.spMax,
-        )
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              'Popular Locations',
+              style: AppTextStyles.heading1,
+            ),
+            SizedBox(width: 12.w),
+            Icon(
+              Icons.location_on,
+              color: Colors.red,
+              size: 40.spMax,
+            )
+          ],
+        ),
       ],
     );
   }
