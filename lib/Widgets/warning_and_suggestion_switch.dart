858d20ed1241399310068c2da282810a74f9e646
1Shibam <hehe.shibam@gmail.com>
2025-02-19 18:59:34 +0530

    create a separate warning and search widget

---
+++ lib/Widgets/warning_and_suggestion_switch.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:my_weather_reader/Widgets/show_warning_dialog.dart';
+import 'package:my_weather_reader/providers/show_serach_suggestion_preference.dart';
+import 'package:my_weather_reader/themes/text_styles.dart';
+
+class WarningAndSuggestionSwitch extends StatelessWidget {
+  const WarningAndSuggestionSwitch({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        InkWell(
+          onTap: () => showWarningDialog(context),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(Icons.info_outline,
+                  color: Colors.red, size: 22.sp),
+              SizedBox(width: 6.w),
+              Text(
+                "Note",
+                style: AppTextStyles.heading2.copyWith(
+                  color: Colors.red,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Consumer(
+          builder: (context, ref, child) {
+            final value =
+                ref.watch(searchSuggestionEnableProvider);
+            return Switch(
+              activeColor: Colors.blue,
+              value: value,
+              onChanged: (currentVal) {
+                ref
+                    .read(searchSuggestionEnableProvider.notifier)
+                    .setSerachState(currentVal);
+              },
+            );
+          },
+        )
+      ],
+    );
+  }
+}
