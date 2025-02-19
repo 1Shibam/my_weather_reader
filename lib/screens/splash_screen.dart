f45e59846c60038552e74b5fee8f21843a6dbc5c
1Shibam <hehe.shibam@gmail.com>
2025-02-19 20:22:46 +0530

    adding splash screen and setting on boarding preference

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -4,29 +4,40 @@ import 'package:go_router/go_router.dart';
 import 'package:my_weather_reader/themes/app_colors.dart';
 
 import 'package:my_weather_reader/themes/text_styles.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Future.delayed(const Duration(seconds: 2), () {
-      if (context.mounted) context.go('/home'); 
+    Future.delayed(const Duration(seconds: 2), () async {
+      final bool whereToGo = await getOnBoardingPreference();
+      if (context.mounted) {
+        whereToGo ? context.go('/home') : context.go('/onBoarding');
+      }
     });
 
     return Scaffold(
-      backgroundColor: AppColors.darkBlue, 
+      backgroundColor: AppColors.darkBlue,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              "L O A D I N G ",
-              style: AppTextStyles.heading1
-            ),
+            Text("L O A D I N G ", style: AppTextStyles.heading1),
           ],
         ),
       ),
     );
   }
+}
+
+Future<bool> getOnBoardingPreference() async {
+  final prefs = await SharedPreferences.getInstance();
+  return prefs.getBool('hasSeenOnBoarding') ?? false;
+}
+
+Future<void> setOnBoardingPreference(bool value) async {
+  final prefs = await SharedPreferences.getInstance();
+  prefs.setBool('hasSeenOnBoarding', value);
 }
