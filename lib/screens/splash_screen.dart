0c90b780429d08e29bda5f1c1e9f7762363f264a
1Shibam <hehe.shibam@gmail.com>
2025-02-19 20:13:21 +0530

    updating splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
+import 'package:go_router/go_router.dart';
+import 'package:my_weather_reader/themes/app_colors.dart';
+
+import 'package:my_weather_reader/themes/text_styles.dart';
+
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Future.delayed(const Duration(seconds: 2), () {
+      if (context.mounted) context.go('/home'); 
+    });
+
+    return Scaffold(
+      backgroundColor: AppColors.darkBlue, 
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "L O A D I N G ",
+              style: AppTextStyles.heading1
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
