c93d59f9909853604c83896570ba599f78371938
1Shibam <hehe.shibam@gmail.com>
2025-02-19 18:31:35 +0530

    remove the warning from the suggested locaiton too

--- lib/Widgets/suggested_location_text.dart
+++ lib/Widgets/suggested_location_text.dart
@@ -12,61 +12,21 @@ class SuggestedLocationsText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            InkWell(
-              onTap: () => showWarningDialog(context),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(Icons.info_outline, color: Colors.red, size: 22.sp),
-                  SizedBox(width: 6.w),
-                  Text(
-                    "Note",
-                    style: AppTextStyles.heading2.copyWith(
-                      color: Colors.red,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Consumer(
-              builder: (context, ref, child) {
-                final value = ref.watch(searchSuggestionEnableProvider);
-                return Switch(
-                  activeColor: Colors.blue,
-                  value: value,
-                  onChanged: (currentVal) {
-                    ref
-                        .read(searchSuggestionEnableProvider.notifier)
-                        .setSerachState(currentVal);
-                  },
-                );
-              },
-            )
-          ],
+        Text(
+          'Suggested Location',
+          style: AppTextStyles.heading1,
         ),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Text(
-              'Suggested Location',
-              style: AppTextStyles.heading1,
-            ),
-            SizedBox(
-              width: 12.w,
-            ),
-            Icon(
-              Icons.location_on,
-              color: Colors.red,
-              size: 40.spMax,
-            )
-          ],
+        SizedBox(
+          width: 12.w,
         ),
+        Icon(
+          Icons.location_on,
+          color: Colors.red,
+          size: 40.spMax,
+        )
       ],
     );
   }
