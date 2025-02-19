1a13a63727fc213717d3df20d67c2e3a64fe7c9d
1Shibam <hehe.shibam@gmail.com>
2025-02-19 11:08:03 +0530

    add a provider to change state that show suggestion while seraching or not

---
+++ lib/providers/show_serach_suggestion_preference.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ShowSearchSuggestionNotifier extends StateNotifier<bool> {
+  ShowSearchSuggestionNotifier() : super(true) {
+    loadSearchState();
+  }
+  Future<void> loadSearchState() async {
+    final prefs = await SharedPreferences.getInstance();
+    state = prefs.getBool('isSearchSuggestionEnabled') ?? true;
+  }
+
+  Future<void> getSerachState(bool value) async {
+    state = value;
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isSearchSuggestionEnabled', value);
+  }
+}
+
+final searchSuggestionEnableProvider =
+    StateNotifierProvider<ShowSearchSuggestionNotifier, bool>(
+        (ref) => ShowSearchSuggestionNotifier());
