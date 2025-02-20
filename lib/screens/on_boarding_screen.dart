6f54fabaae06f3173c773446568f0527080e5acd
1Shibam <hehe.shibam@gmail.com>
2025-02-20 18:00:58 +0530

    updating the onboaring screen

--- lib/screens/on_boarding_screen.dart
+++ lib/screens/on_boarding_screen.dart
@@ -4,7 +4,6 @@ import 'package:my_weather_reader/themes/app_colors.dart';
 import 'package:my_weather_reader/themes/text_styles.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-
 class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({super.key});
 
@@ -18,69 +17,15 @@ class OnBoardingScreenState extends State<OnBoardingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          PageView(
-            controller: _controller,
-            children: [
-              _buildPage(
-                image: 'assets/weather_1.png',
-                title: 'Stay Updated with the Weather',
-                description: 'Get real-time weather updates for your location.',
-              ),
-              _buildPage(
-                image: 'assets/weather_2.png',
-                title: 'Search Any Location',
-                description: 'Find weather details by name or coordinates.',
-              ),
-              _buildPage(
-                image: 'assets/weather_3.png',
-                title: '5-Day Forecast',
-                description: 'Plan ahead with a 5-day weather forecast.',
-              ),
-             
-            ],
-          ),
-          Positioned(
-            bottom: 50.h,
-            left: 0,
-            right: 0,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SmoothPageIndicator(
-                  controller: _controller,
-                  count: 4,
-                  effect: ExpandingDotsEffect(
-                    activeDotColor: AppColors.skyBlue,
-                    dotColor: Colors.white54,
-                    dotHeight: 8.h,
-                    dotWidth: 8.w,
-                  ),
-                ),
-                SizedBox(height: 20.h),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: AppColors.waterBlue,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.r),
-                    ),
-                    padding: EdgeInsets.symmetric(vertical: 12.h, horizontal: 30.w),
-                  ),
-                  onPressed: () {
-                    // Navigate to the main screen
-                  },
-                  child: Text('Get Started', style: AppTextStyles.bold),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
+        body: PageView(
+      controller: _controller,
+    ));
   }
 
-  Widget _buildPage({required String image, required String title, required String description}) {
+  Widget _buildPage(
+      {required String image,
+      required String title,
+      required String description}) {
     return Container(
       color: AppColors.darkBlue,
       child: Column(
@@ -88,25 +33,64 @@ class OnBoardingScreenState extends State<OnBoardingScreen> {
         children: [
           Image.asset(image, width: 250.w, height: 250.h),
           SizedBox(height: 20.h),
-          Text(title, style: AppTextStyles.heading1, textAlign: TextAlign.center),
+          Text(title,
+              style: AppTextStyles.heading1, textAlign: TextAlign.center),
           SizedBox(height: 10.h),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 30.w),
-            child: Text(description, style: AppTextStyles.regular, textAlign: TextAlign.center),
+            child: Text(description,
+                style: AppTextStyles.regular, textAlign: TextAlign.center),
           ),
         ],
       ),
     );
   }
 }
+
 class BuildOnBoardingPages extends StatelessWidget {
-  const BuildOnBoardingPages({super.key});
+  final String imageUrl;
+  final String title;
+  final String description;
+  final bool hasButton;
+  const BuildOnBoardingPages(
+      {super.key,
+      required this.imageUrl,
+      required this.title,
+      required this.description,
+      this.hasButton = false});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            imageUrl,
+            width: 200.w,
+            height: 200.h,
+          ),
+          SizedBox(height: 20.h),
+          Text(
+            title,
+            style: AppTextStyles.heading1,
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 10.h,
+          ),
+          Padding(
+            padding: EdgeInsets.all(12.w),
+            child: Text(
+              description,
+              style: AppTextStyles.regular,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          hasButton ? Container(
+            
+          ) : const SizedBox.shrink()
+        ],
       ),
     );
   }
