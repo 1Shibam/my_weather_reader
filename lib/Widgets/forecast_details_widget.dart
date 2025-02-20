3e40cb39e7c63345e7dcd1f50a3b6e08619718b6
1Shibam <hehe.shibam@gmail.com>
2025-02-20 18:59:07 +0530

    adds fiorecast details widget

---
+++ lib/Widgets/forecast_details_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ForecastDetailsWidget extends ConsumerStatefulWidget {
+  const ForecastDetailsWidget({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _ForecastDetailsWidgetState();
+}
+
+class _ForecastDetailsWidgetState extends ConsumerState<ForecastDetailsWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
