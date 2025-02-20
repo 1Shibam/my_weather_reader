aabcbc83cd9724e0aec5c600d2f12cef15a5c390
1Shibam <hehe.shibam@gmail.com>
2025-02-20 18:50:02 +0530

    minor text size and padding change

--- lib/screens/on_boarding_screen.dart
+++ lib/screens/on_boarding_screen.dart
@@ -22,73 +22,76 @@ class OnBoardingScreenState extends State<OnBoardingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: AppColors.darkBlue,
         body: Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
-      child: Stack(
-        children: [
-          Align(
-            alignment: Alignment.topRight,
-            child: TextButton(
-                onPressed: () {
-                  //setting prefernce of on boarding page to true it wont show up again
-                  setOnBoardingPreference(true);
-                  context.go('/');
-                },
-                child: Text(
-                  'S K I P',
-                  style: AppTextStyles.heading2,
-                )),
-          ),
-          PageView(
-            controller: _controller,
+          padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 40.h),
+          child: Stack(
             children: [
-              const BuildOnBoardingPages(
-                  imageUrl: 'assets/animations/stay updated with weather.png',
-                  title: 'Stay Updated with the Weather',
-                  description:
-                      'Get real-time weather updates for your location.'),
-              const BuildOnBoardingPages(
-                  imageUrl: 'assets/animations/location pin final.gif',
-                  title: 'Search Any Location',
-                  description: 'Find weather details by name or coordinates.'),
-              BuildOnBoardingPages(
-                imageUrl: 'assets/animations/forecast.png',
-                title: '5-Day Forecast',
-                description: 'Plan ahead with a 5-day weather forecast.',
-                hasButton: true,
-                onPressed: () {
-                  //set the preference the on boarding page wont show up after this 
-                  //same thing will happen when the top skip text button will be pressed 
-                  setOnBoardingPreference(true);
-                  context.go('/');
-                },
+              Align(
+                alignment: Alignment.topRight,
+                child: TextButton(
+                    onPressed: () {
+                      //setting prefernce of on boarding page to true it wont show up again
+                      setOnBoardingPreference(true);
+                      context.go('/');
+                    },
+                    child: Text(
+                      'S K I P',
+                      style: AppTextStyles.heading2,
+                    )),
+              ),
+              PageView(
+                controller: _controller,
+                children: [
+                  const BuildOnBoardingPages(
+                      imageUrl:
+                          'assets/animations/stay updated with weather.png',
+                      title: 'Stay Updated with the Weather',
+                      description:
+                          'Get real-time weather updates for your location.'),
+                  const BuildOnBoardingPages(
+                      imageUrl: 'assets/animations/location pin final.gif',
+                      title: 'Search Any Location',
+                      description:
+                          'Find weather details by name or coordinates.'),
+                  BuildOnBoardingPages(
+                    imageUrl: 'assets/animations/forecast.png',
+                    title: '5-Day Forecast',
+                    description: 'Plan ahead with a 5-day weather forecast.',
+                    hasButton: true,
+                    onPressed: () {
+                      //set the preference the on boarding page wont show up after this
+                      //same thing will happen when the top skip text button will be pressed
+                      setOnBoardingPreference(true);
+                      context.go('/');
+                    },
+                  ),
+                ],
+              ),
+              Positioned(
+                bottom: 100.h,
+                left: 0,
+                right: 0,
+                child: Center(
+                  child: SmoothPageIndicator(
+                    onDotClicked: (index) {
+                      _controller.animateToPage(index,
+                          duration: const Duration(milliseconds: 300),
+                          curve: Curves.easeInOutCirc);
+                    },
+                    controller: _controller,
+                    count: 3,
+                    effect: ExpandingDotsEffect(
+                      activeDotColor: AppColors.skyBlue,
+                      dotColor: Colors.white54,
+                      dotHeight: 8.h,
+                      dotWidth: 8.w,
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
-          Positioned(
-            bottom: 100.h,
-            left: 0,
-            right: 0,
-            child: SmoothPageIndicator(
-              onDotClicked: (index) {
-                _controller.animateToPage(index,
-                    duration: const Duration(milliseconds: 300),
-                    curve: Curves.easeInOutCirc);
-              },
-              controller: _controller,
-              count: 3,
-              effect: ExpandingDotsEffect(
-                activeDotColor: AppColors.skyBlue,
-                dotColor: Colors.white54,
-                dotHeight: 8.h,
-                dotWidth: 8.w,
-              ),
-            ),
-          ),
-        ],
-      ),
-    ));
+        ));
   }
 }
-
-
