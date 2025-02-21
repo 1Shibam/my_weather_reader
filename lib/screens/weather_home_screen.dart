5e9bdbf238496aad5c1e50da12dd792ad0550f58
1Shibam <hehe.shibam@gmail.com>
2025-02-21 18:44:07 +0530

    minor fix

--- lib/screens/weather_home_screen.dart
+++ lib/screens/weather_home_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:weather_reader/Widgets/main_widgets/app_bar_widget.dart';
 import 'package:weather_reader/Widgets/main_widgets/drawer_widget.dart';
-import 'package:weather_reader/Widgets/data_widgets/forecast_details_widget.dart';
 import 'package:weather_reader/Widgets/main_widgets/search_location_widget.dart';
 import 'package:weather_reader/Widgets/data_widgets/weather_details.dart';
 import 'package:weather_reader/providers/data_providers/weather_service_provider.dart';
