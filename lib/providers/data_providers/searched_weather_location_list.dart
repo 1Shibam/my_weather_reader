7058222dcd12848377a19c9cddcfd522604a1362
1Shibam <hehe.shibam@gmail.com>
2025-02-21 09:46:59 +0530

    update the searchd weather location list

--- lib/providers/data_providers/searched_weather_location_list.dart
+++ lib/providers/data_providers/searched_weather_location_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_weather_reader/models/weather%20model/weather_data_model.dart';
 import 'package:my_weather_reader/services/weather_database_service.dart';
-import 'package:my_weather_reader/models/weather%20model/weather_data.dart';
 
 class SearchedWeatherLocationList
     extends StateNotifier<List<WeatherDataModel>> {
