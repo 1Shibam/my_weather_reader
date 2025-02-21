03a036b962addd31f5c4cb67262c8a27180c8967
1Shibam <hehe.shibam@gmail.com>
2025-02-21 13:43:01 +0530

    minor fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:weather_reader/providers/data_providers/weather_service_provider.dart';
 import 'package:weather_reader/themes/theme.dart';
 
 import 'router/router_config.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Ensure async services initialize
-  final container = ProviderContainer();
-
-  // Wait for the location and weather data to load before app starts
-  await container
-      .read(weatherServiceNotifierProvider.notifier)
-      .initializeWeatherStates();
 
   runApp(
-    UncontrolledProviderScope(
-      container: container,
-      child: const MyApp(),
+    const ProviderScope(
+      child: MyApp(),
     ),
   );
 }
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized(); // Ensure async services initialize
+//   final container = ProviderContainer();
+
+//   // Wait for the location and weather data to load before app starts
+//   await container
+//       .read(weatherServiceNotifierProvider.notifier)
+//       .initializeWeatherStates();
+
+//   runApp(
+//     UncontrolledProviderScope(
+//       container: container,
+//       child: const MyApp(),
+//     ),
+//   );
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
