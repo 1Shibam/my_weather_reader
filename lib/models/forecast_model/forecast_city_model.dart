ca64ef5f779fcca4839341936a6689b3c457491a
1Shibam <hehe.shibam@gmail.com>
2025-02-22 18:26:42 +0530

    update city model

--- lib/models/forecast_model/forecast_city_model.dart
+++ lib/models/forecast_model/forecast_city_model.dart
@@ -1,11 +1,15 @@
+import 'package:weather_reader/models/forecast_model/forecast_list.dart';
+
 class ForecastModel {
   final String cityName;
   final int timezone;
   final int sunrise;
   final int sunset;
+  final List<ForecastList> allForecasts;
   ForecastModel(
       {required this.cityName,
       required this.timezone,
       required this.sunrise,
-      required this.sunset});
+      required this.sunset,
+      required this.allForecasts});
 }
