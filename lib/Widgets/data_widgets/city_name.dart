b4648fc3e0f79140ad02611d87816f611e5c5fb7
1Shibam <hehe.shibam@gmail.com>
2025-02-22 14:30:03 +0530

    fixed city name widget

--- lib/Widgets/data_widgets/city_name.dart
+++ lib/Widgets/data_widgets/city_name.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:weather_reader/Widgets/main_widgets/other_weather_details_expansion_tile.dart';
 import 'package:weather_reader/themes/app_colors.dart';
 import 'package:weather_reader/themes/text_styles.dart';
 
-
 class LocationName extends StatelessWidget {
   final String locationName;
-  const LocationName({super.key, required this.locationName});
+  final int currentTime;
+  const LocationName(
+      {super.key, required this.locationName, required this.currentTime});
 
   @override
   Widget build(BuildContext context) {
+    String time = formatTime(currentTime);
     return ListTile(
       title: Row(
         children: [
@@ -29,9 +32,21 @@ class LocationName extends StatelessWidget {
       ),
       tileColor: AppColors.waterBlue,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.r)),
-      subtitle: Text(
-        locationName,
-        style: AppTextStyles.heading1.copyWith(fontSize: 28.sp),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            locationName,
+            style: AppTextStyles.heading1.copyWith(fontSize: 28.sp),
+          ),
+          SizedBox(
+            height: 4.h,
+          ),
+          Text(
+            time,
+            style: AppTextStyles.heading1,
+          ),
+        ],
       ),
     );
   }
