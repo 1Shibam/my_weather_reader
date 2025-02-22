7189249800a1793cc6659731fc20d4a6c4cf7da8
1Shibam <hehe.shibam@gmail.com>
2025-02-22 18:07:19 +0530

    forecast list model

--- lib/models/forecast_model/forecast_list.dart
+++ lib/models/forecast_model/forecast_list.dart
@@ -1,73 +1,47 @@
-
 class ForecastList {
-  int? dt;
+
   Main? main;
   List<Weather>? weather;
   Clouds? clouds;
   Wind? wind;
-  int? visibility;
-  int? pop;
-  Sys? sys;
+
+
+
   String? dtTxt;
 
-  ForecastList({this.dt, this.main, this.weather, this.clouds, this.wind, this.visibility, this.pop, this.sys, this.dtTxt});
+  ForecastList(
+      {
+      this.main,
+      this.weather,
+      this.clouds,
+      this.wind,
+
+
+      this.dtTxt});
 
   ForecastList.fromJson(Map<String, dynamic> json) {
-    if(json["dt"] is int) {
-      dt = json["dt"];
-    }
-    if(json["main"] is Map) {
-      main = json["main"] == null ? null : Main.fromJson(json["main"]);
-    }
-    if(json["weather"] is List) {
-      weather = json["weather"] == null ? null : (json["weather"] as List).map((e) => Weather.fromJson(e)).toList();
-    }
-    if(json["clouds"] is Map) {
-      clouds = json["clouds"] == null ? null : Clouds.fromJson(json["clouds"]);
-    }
-    if(json["wind"] is Map) {
-      wind = json["wind"] == null ? null : Wind.fromJson(json["wind"]);
-    }
-    if(json["visibility"] is int) {
-      visibility = json["visibility"];
-    }
-    if(json["pop"] is int) {
-      pop = json["pop"];
-    }
-    if(json["sys"] is Map) {
-      sys = json["sys"] == null ? null : Sys.fromJson(json["sys"]);
-    }
-    if(json["dt_txt"] is String) {
-      dtTxt = json["dt_txt"];
-    }
-  }
 
-  static List<ForecastList> fromList(List<Map<String, dynamic>> list) {
-    return list.map(ForecastList.fromJson).toList();
+    main = json["main"] != null ? Main.fromJson(json["main"]) : null;
+    weather = json["weather"] != null
+        ? (json["weather"] as List).map((e) => Weather.fromJson(e)).toList()
+        : null;
+    clouds = json["clouds"] != null ? Clouds.fromJson(json["clouds"]) : null;
+    wind = json["wind"] != null ? Wind.fromJson(json["wind"]) : null;
+
+    dtTxt = json["dt_txt"];
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data["dt"] = dt;
-    if(main != null) {
-      data["main"] = main?.toJson();
-    }
-    if(weather != null) {
-      data["weather"] = weather?.map((e) => e.toJson()).toList();
-    }
-    if(clouds != null) {
-      data["clouds"] = clouds?.toJson();
-    }
-    if(wind != null) {
-      data["wind"] = wind?.toJson();
-    }
-    data["visibility"] = visibility;
-    data["pop"] = pop;
-    if(sys != null) {
-      data["sys"] = sys?.toJson();
-    }
-    data["dt_txt"] = dtTxt;
-    return data;
+    return {
+
+      "main": main?.toJson(),
+      "weather": weather?.map((e) => e.toJson()).toList(),
+      "clouds": clouds?.toJson(),
+      "wind": wind?.toJson(),
+
+
+      "dt_txt": dtTxt,
+    };
   }
 }
 
@@ -77,19 +51,11 @@ class Sys {
   Sys({this.pod});
 
   Sys.fromJson(Map<String, dynamic> json) {
-    if(json["pod"] is String) {
-      pod = json["pod"];
-    }
-  }
-
-  static List<Sys> fromList(List<Map<String, dynamic>> list) {
-    return list.map(Sys.fromJson).toList();
+    pod = json["pod"];
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data["pod"] = pod;
-    return data;
+    return {"pod": pod};
   }
 }
 
@@ -101,27 +67,17 @@ class Wind {
   Wind({this.speed, this.deg, this.gust});
 
   Wind.fromJson(Map<String, dynamic> json) {
-    if(json["speed"] is double) {
-      speed = json["speed"];
-    }
-    if(json["deg"] is int) {
-      deg = json["deg"];
-    }
-    if(json["gust"] is double) {
-      gust = json["gust"];
-    }
-  }
-
-  static List<Wind> fromList(List<Map<String, dynamic>> list) {
-    return list.map(Wind.fromJson).toList();
+    speed = json["speed"]?.toDouble();
+    deg = json["deg"];
+    gust = json["gust"]?.toDouble();
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data["speed"] = speed;
-    data["deg"] = deg;
-    data["gust"] = gust;
-    return data;
+    return {
+      "speed": speed,
+      "deg": deg,
+      "gust": gust,
+    };
   }
 }
 
@@ -131,19 +87,11 @@ class Clouds {
   Clouds({this.all});
 
   Clouds.fromJson(Map<String, dynamic> json) {
-    if(json["all"] is int) {
-      all = json["all"];
-    }
-  }
-
-  static List<Clouds> fromList(List<Map<String, dynamic>> list) {
-    return list.map(Clouds.fromJson).toList();
+    all = json["all"];
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data["all"] = all;
-    return data;
+    return {"all": all};
   }
 }
 
@@ -156,31 +104,19 @@ class Weather {
   Weather({this.id, this.main, this.description, this.icon});
 
   Weather.fromJson(Map<String, dynamic> json) {
-    if(json["id"] is int) {
-      id = json["id"];
-    }
-    if(json["main"] is String) {
-      main = json["main"];
-    }
-    if(json["description"] is String) {
-      description = json["description"];
-    }
-    if(json["icon"] is String) {
-      icon = json["icon"];
-    }
-  }
-
-  static List<Weather> fromList(List<Map<String, dynamic>> list) {
-    return list.map(Weather.fromJson).toList();
+    id = json["id"];
+    main = json["main"];
+    description = json["description"];
+    icon = json["icon"];
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data["id"] = id;
-    data["main"] = main;
-    data["description"] = description;
-    data["icon"] = icon;
-    return data;
+    return {
+      "id": id,
+      "main": main,
+      "description": description,
+      "icon": icon,
+    };
   }
 }
 
@@ -195,53 +131,40 @@ class Main {
   int? humidity;
   double? tempKf;
 
-  Main({this.temp, this.feelsLike, this.tempMin, this.tempMax, this.pressure, this.seaLevel, this.grndLevel, this.humidity, this.tempKf});
+  Main(
+      {this.temp,
+      this.feelsLike,
+      this.tempMin,
+      this.tempMax,
+      this.pressure,
+      this.seaLevel,
+      this.grndLevel,
+      this.humidity,
+      this.tempKf});
 
   Main.fromJson(Map<String, dynamic> json) {
-    if(json["temp"] is double) {
-      temp = json["temp"];
-    }
-    if(json["feels_like"] is double) {
-      feelsLike = json["feels_like"];
-    }
-    if(json["temp_min"] is double) {
-      tempMin = json["temp_min"];
-    }
-    if(json["temp_max"] is double) {
-      tempMax = json["temp_max"];
-    }
-    if(json["pressure"] is int) {
-      pressure = json["pressure"];
-    }
-    if(json["sea_level"] is int) {
-      seaLevel = json["sea_level"];
-    }
-    if(json["grnd_level"] is int) {
-      grndLevel = json["grnd_level"];
-    }
-    if(json["humidity"] is int) {
-      humidity = json["humidity"];
-    }
-    if(json["temp_kf"] is double) {
-      tempKf = json["temp_kf"];
-    }
-  }
-
-  static List<Main> fromList(List<Map<String, dynamic>> list) {
-    return list.map(Main.fromJson).toList();
+    temp = json["temp"]?.toDouble();
+    feelsLike = json["feels_like"]?.toDouble();
+    tempMin = json["temp_min"]?.toDouble();
+    tempMax = json["temp_max"]?.toDouble();
+    pressure = json["pressure"];
+    seaLevel = json["sea_level"];
+    grndLevel = json["grnd_level"];
+    humidity = json["humidity"];
+    tempKf = json["temp_kf"]?.toDouble();
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data["temp"] = temp;
-    data["feels_like"] = feelsLike;
-    data["temp_min"] = tempMin;
-    data["temp_max"] = tempMax;
-    data["pressure"] = pressure;
-    data["sea_level"] = seaLevel;
-    data["grnd_level"] = grndLevel;
-    data["humidity"] = humidity;
-    data["temp_kf"] = tempKf;
-    return data;
+    return {
+      "temp": temp,
+      "feels_like": feelsLike,
+      "temp_min": tempMin,
+      "temp_max": tempMax,
+      "pressure": pressure,
+      "sea_level": seaLevel,
+      "grnd_level": grndLevel,
+      "humidity": humidity,
+      "temp_kf": tempKf,
+    };
   }
 }
