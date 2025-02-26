09054ef94bb2460aae523a58bd0fdf160b7cf34d
1Shibam <hehe.shibam@gmail.com>
2025-02-26 16:59:18 +0530

    fix the database for storing history

--- lib/database/weather_database.dart
+++ lib/database/weather_database.dart
@@ -6,6 +6,7 @@ import 'package:sqflite/sqflite.dart';
 final weatherDataBaseProvider = FutureProvider<Database>((ref) async {
   final dbPath = await getApplicationDocumentsDirectory();
   String pathDirectory = join(dbPath.path, 'weather.db');
+  await deleteDatabase(pathDirectory);
   return openDatabase(
     pathDirectory,
     version: 1,
@@ -15,8 +16,8 @@ final weatherDataBaseProvider = FutureProvider<Database>((ref) async {
             weatherID INTEGER PRIMARY KEY AUTOINCREMENT,
             cityName TEXT NOT NULL,
             country TEXT NOT NULL,
-            REAL lat,
-            REAL lon,
+            lat REAL,
+            lon REAL,
             temperature REAL,
             tempMin REAL,
             tempMax REAL,
@@ -37,72 +38,4 @@ final weatherDataBaseProvider = FutureProvider<Database>((ref) async {
 });
 
 
-/*
 
-class WeatherData {
-  final String cityName;
-  final double temperature;
-  final double tempMin;
-  final double tempMax;
-  final int humidity;
-  final double windSpeed;
-  final int windDeg;
-  final String description;
-  final int cloudCoverage;
-  final int pressure;
-  final int sunrise; 
-  final int sunset; 
-  final int currentTime; 
-  final int timezone; 
-
-  WeatherData({
-    required this.cityName,
-    required this.temperature,
-    required this.tempMin,
-    required this.tempMax,
-    required this.humidity,
-    required this.windSpeed,
-    required this.windDeg,
-    required this.description,
-    required this.cloudCoverage,
-    required this.pressure,
-    required this.sunrise,
-    required this.sunset,
-    required this.currentTime,
-    required this.timezone,
-  });
-
-  factory WeatherData.fromJson(Map<String, dynamic> json) {
-    final temp = json['main']['temp'].toDouble();
-    final tempMin = json['main']['temp_min'].toDouble();
-    final tempMax = json['main']['temp_max'].toDouble();
-    final humidity = json['main']['humidity'];
-    final windSpeed = json['wind']['speed'].toDouble();
-    final windDeg = json['wind']['deg'];
-    final description = json['weather'][0]['description'];
-    final cloudCoverage = json['clouds']['all'];
-    final pressure = json['main']['pressure'];
-
-    final sunrise = json['sys']['sunrise'];
-    final sunset = json['sys']['sunset'];
-    final currentTime = json['dt']; 
-
-    return WeatherData(
-      cityName: json['name'],
-      temperature: temp,
-      tempMin: tempMin,
-      tempMax: tempMax,
-      humidity: humidity,
-      windSpeed: windSpeed,
-      windDeg: windDeg,
-      description: description,
-      cloudCoverage: cloudCoverage,
-      pressure: pressure,
-      sunrise: sunrise,
-      sunset: sunset,
-      currentTime: currentTime,
-      timezone: json['timezone'],
-    );
-  }
-}
-*/
