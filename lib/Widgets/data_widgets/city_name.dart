c8c53f6b2c7c3c917ac47459e1edf209301d718e
1Shibam <hehe.shibam@gmail.com>
2025-02-28 00:32:12 +0530

    add key

--- lib/Widgets/data_widgets/city_name.dart
+++ lib/Widgets/data_widgets/city_name.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:weather_reader/Widgets/main_widgets/other_weather_details_expansion_tile.dart';
 import 'package:weather_reader/themes/app_colors.dart';
 import 'package:weather_reader/themes/text_styles.dart';
 
-class LocationName extends StatelessWidget {
+class LocationName extends ConsumerWidget {
   final String locationName;
   final String country;
   final int currentTime;
@@ -12,9 +13,10 @@ class LocationName extends StatelessWidget {
       {super.key, required this.locationName, required this.currentTime, required this.country});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
     String time = formatTime(currentTime);
     return ListTile(
+      key: ValueKey(locationName),
       title: Row(
         children: [
           Text(
